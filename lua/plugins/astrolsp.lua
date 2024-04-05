d35e5d7c3a4ba9448c06c7d79a476c46b399c832
Serhii Shvets <wert2all@gmail.com>
2024-04-05 19:20:40 +0300

    Initial commit

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -12,6 +12,7 @@ return {
   opts = {
     -- Configuration table of features provided by AstroLSP
     features = {
+      autoformat = true, -- enable or disable auto formatting on start
       codelens = true, -- enable/disable codelens refresh on start
       inlay_hints = false, -- enable/disable inlay hints on start
       semantic_tokens = true, -- enable/disable semantic token highlighting
@@ -80,6 +81,7 @@ return {
     -- mappings to be set up on attaching of a language server
     mappings = {
       n = {
+        gl = { function() vim.diagnostic.open_float() end, desc = "Hover diagnostics" },
         -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
         gD = {
           function() vim.lsp.buf.declaration() end,
