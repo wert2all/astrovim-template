e0faac7d37ccd05fc92ee5eedd5acb6af3b91f66
Serhii Shvets <wert2all@gmail.com>
2024-04-05 19:45:14 +0300

    add treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,30 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
+  opts = function(_, opts)
+    -- add more things to the ensure_installed table protecting against community packs modifying it
+    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      "bash",
+      "css",
+      "diff",
+      "dockerfile",
+      "html",
+      "gitignore",
+      "graphql",
+      "javascript",
+      "jsdoc",
+      "json",
       "lua",
+      "luadoc",
+      "php",
+      "scss",
+      "tsx",
+      "typescript",
       "vim",
-      -- add more arguments for adding more treesitter parsers
-    },
-  },
+      "vimdoc",
+      "yaml",
+    })
+  end,
 }
