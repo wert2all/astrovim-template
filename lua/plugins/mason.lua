d093f5f1447f02fcf2b1d57410fce1b5957282fc
wert2all <wert2all@gmail.com>
2024-04-10 11:31:37 +0300

    fix: add rust LSP

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,6 +2,8 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Mason
 
+-- Customize Mason
+
 ---@type LazySpec
 return {
   -- use mason-tool-installer for automatically installing Mason packages
@@ -16,6 +18,24 @@ return {
 
         -- install formatters
         "stylua",
+
+        -- install debuggers
+        "debugpy",
+
+        -- install any other package
+        "tree-sitter-cli",
+      },
+    },
+  -- use mason-tool-installer for automatically installing Mason packages
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    -- overrides `require("mason-tool-installer").setup(...)`
+    opts = {
+      -- Make sure to use the names found in `:Mason`
+      ensure_installed = {
+        -- install language servers
+        "lua-language-server",
+
+        -- install formatters
 
         -- install debuggers
         "debugpy",
