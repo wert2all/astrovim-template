a338d4d11335ea5f40e24908bc8a21fdf6fdc44e
wert2all <wert2all@gmail.com>
2024-06-12 19:19:24 +0300

    fix: merge commits from origin template

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,9 +3,8 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+  opts = {
+    ensure_installed = {
       "bash",
       "css",
       "diff",
@@ -26,6 +25,6 @@ return {
       "vimdoc",
       "yaml",
       "go",
-    })
-  end,
+    },
+  },
 }
