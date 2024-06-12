59c67c1c44ae1dc911c0d47f4a4a12e2640076cb
wert2all <wert2all@gmail.com>
2024-06-13 02:00:52 +0300

    fix: fix auto-save and codeium

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -1,11 +1,34 @@
----@type LazySpec
-
 return {
   "okuuva/auto-save.nvim",
-  cmd = "ASToggle", -- optional for lazy loading on command
+  cmd = "ASToggle",
   event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
-  opts = { enabled = true },
-  keys = {
-    { "<leader>n", ":ASToggle<CR>", desc = "Toggle auto-save" },
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader>uo"] = {
+              desc = "Toggle auto-save",
+              function() vim.cmd "ASToggle" end,
+            },
+          },
+        },
+      },
+    },
+  },
+  opts = {
+    enabled = true,
+    execution_message = {
+      enabled = true,
+      message = function() -- message to print on save
+        return ("AutoSave: " .. vim.fn.strftime "%H:%M:%S")
+      end,
+    },
+    trigger_events = { -- See :h events
+      immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
+      defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+      cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
+    },
   },
 }
--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,18 +1,61 @@
 ---@type LazySpec
 return {
-  "Exafunction/codeium.vim",
-  event = "User AstroFile",
-  config = function()
-    vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
-    vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
-    vim.keymap.set("i", "<c-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
-    vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
-    vim.keymap.set("n", "<leader>;", function()
-      if vim.g.codeium_enabled == true then
-        vim.cmd "CodeiumDisable"
-      else
-        vim.cmd "CodeiumEnable"
-      end
-    end, { noremap = true, desc = "Toggle Codeium active" })
-  end,
+  {
+    "Exafunction/codeium.vim",
+    event = "User AstroFile",
+    opts = {
+      enable_chat = false,
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+      {
+        "AstroNvim/astrocore",
+        opts = {
+          mappings = {
+            n = {
+              ["<Leader>;"] = {
+                name = " Codeium",
+              },
+              ["<Leader>;t"] = {
+                desc = "Toggle Codeium active",
+                function()
+                  if vim.g.codeium_enabled == true then
+                    vim.cmd "CodeiumDisable"
+                  else
+                    vim.cmd "CodeiumEnable"
+                  end
+                end,
+              },
+            },
+          },
+        },
+      },
+    },
+    config = function()
+      vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+      vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
+      vim.keymap.set("i", "<c-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
+      vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    dependencies = { "Exafunction/codeium.nvim" },
+    opts = function(_, opts)
+      -- Inject codeium into cmp sources, with high priority
+      table.insert(opts.sources, 1, {
+        name = "codeium",
+        group_index = 1,
+        priority = 10000,
+      })
+    end,
+  },
+  {
+    "onsails/lspkind.nvim",
+    optional = true,
+    -- Adds icon for codeium using lspkind
+    opts = function(_, opts) opts.symbol_map = { Codeium = "" } end,
+  },
 }
