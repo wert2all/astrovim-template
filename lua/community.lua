b93d4dc10b56eceab0b38bb3c0937e589fb73d32
Serhii Shvets <wert2all@gmail.com>
2024-07-15 12:51:26 +0300

    feat: add ansible

--- lua/community.lua
+++ lua/community.lua
@@ -12,6 +12,7 @@ return {
   { import = "astrocommunity.pack.php" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.ansible" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.media.vim-wakatime" },
