d34b8cd4c9aadaa5e4055eaf0cd6325f16a08daf
Serhii Shvets <wert2all@gmail.com>
2024-08-21 14:29:10 +0300

    fix: fix neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,17 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     window = { position = "right" },
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_by_name = {
+          ".git",
+        },
+        always_show = {
+          ".env",
+        },
+      },
+    },
     close_if_last_window = false,
   },
 }
