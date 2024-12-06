4d578372a888cd1af1aea514c8926576ca96a6da
wert2all <wert2all@gmail.com>
2024-12-06 14:55:45 +0200

    fix: remove status line

--- lua/plugins/disable-tabline.lua
+++ lua/plugins/disable-tabline.lua
@@ -4,13 +4,15 @@ return {
     ---@param opts AstroCoreOpts
     opts = function(_, opts)
       if vim.tbl_get(opts, "options", "opt", "showtabline") then opts.options.opt.showtabline = nil end
+      if vim.tbl_get(opts, "options", "opt", "laststatus") then opts.options.opt.laststatus = 0 end
     end,
   },
   {
     "rebelot/heirline.nvim",
     opts = function(_, opts)
       opts.tabline = nil -- remove tabline
-      opts.winbar = nil -- remove winbar
+      -- opts.winbar = nil -- remove winbar
+      opts.statusline = nil -- rmove statusline
     end,
   },
 }
