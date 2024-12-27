3cdad6fb7ff3339254b63e2c567113faab76ab8c
Serhii Shvets <wert2all@gmail.com>
2024-12-27 02:43:12 +0200

    fix: add borderless to colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,51 @@ return {
   { "sainnhe/gruvbox-material", lazy = true },
   { "wilmanbarrios/palenight.nvim", lazy = true },
   { "samharju/synthweave.nvim", lazy = true },
-  { "folke/tokyonight.nvim", lazy = true },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    config = function()
+      require("tokyonight").setup {
+        on_highlights = function(hl, c)
+          local prompt = "#2d3149"
+          hl.TelescopeNormal = {
+            bg = c.bg_dark,
+            fg = c.fg_dark,
+          }
+          hl.TelescopeBorder = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+          hl.TelescopePromptNormal = {
+            bg = prompt,
+          }
+          hl.TelescopePromptBorder = {
+            bg = prompt,
+            fg = prompt,
+          }
+          hl.TelescopePromptTitle = {
+            bg = prompt,
+            fg = prompt,
+          }
+          hl.TelescopePreviewTitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+          hl.TelescopeResultsTitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+
+          -- notify borders
+          hl.NotifyERRORBorder = { fg = c.bg }
+          hl.NotifyWARNBorder = { fg = c.bg }
+          hl.NotifyINFOBorder = { fg = c.bg }
+          hl.NotifyDEBUGBorder = { fg = c.bg }
+          hl.NotifyTRACEBorder = { fg = c.bg }
+        end,
+      }
+    end,
+  },
   { "rebelot/kanagawa.nvim", lazy = true },
   { "vague2k/vague.nvim", lazy = true },
   { "EdenEast/nightfox.nvim", lazy = true },
