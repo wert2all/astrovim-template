6f1a2e09f9e6b19759f0d569e75ad1cfd3560555
wert2all <wert2all@gmail.com>
2025-01-08 11:30:43 +0200

    fix: remove borderless

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -5,27 +5,4 @@ return {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
   },
-  opts = {
-    views = {
-      -- Clean cmdline_popup + palette
-      cmdline_popup = {
-        position = {
-          row = 10,
-          col = "50%",
-        },
-        border = {
-          style = "none",
-          padding = { 1, 1 },
-        },
-        size = {
-          min_width = 60,
-          width = "auto",
-          height = "auto",
-        },
-        win_options = {
-          winhighlight = { NormalFloat = "NormalFloat", FloatBorder = "FloatBorder" },
-        },
-      },
-    },
-  },
 }
