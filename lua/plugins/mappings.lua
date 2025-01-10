09eed3d544b996272cb9bb6231a3509a7331c3ab
Serhii Shvets <wert2all@gmail.com>
2025-01-10 03:02:20 +0200

    feat: add zen-mode

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -4,8 +4,8 @@ return {
   opts = {
     mappings = {
       n = {
-        ["<Leader>fB"] = { "<cmd>Telescope registers<cr>", desc = "Resisters" },
-        ["<Leader>fr"] = { "<cmd>Telescope oldfiles<cr>", desc = "Recent files" },
+        -- ["<Leader>fB"] = { "<cmd>Telescope registers<cr>", desc = "Resisters" },
+        -- ["<Leader>fr"] = { "<cmd>Telescope oldfiles<cr>", desc = "Recent files" },
         ["<Leader>c"] = {
           function()
             local bufs = vim.fn.getbufinfo { buflisted = 1 }
@@ -14,10 +14,10 @@ return {
           end,
           desc = "Close buffer",
         },
-        ["<Leader>uo"] = {
-          function() vim.cmd "ASToggle" end,
-          desc = "Toggle auto-save",
-        },
+        -- ["<Leader>uo"] = {
+        --   function() vim.cmd "ASToggle" end,
+        --   desc = "Toggle auto-save",
+        -- },
         ["<Leader>e"] = {
           "<cmd>Neotree float reveal<cr>",
           desc = "Toggle Explorer",
