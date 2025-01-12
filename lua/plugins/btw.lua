6437ca0d9020c0883d64131c9a44b3da67e62e40
wert2all <wert2all@gmail.com>
2025-01-12 14:52:35 +0200

    fix: move keybindings

--- lua/plugins/btw.lua
+++ lua/plugins/btw.lua
@@ -1,6 +1,22 @@
+local function openDashboard()
+  local bufs = vim.fn.getbufinfo { buflisted = 1 }
+  require("astrocore.buffer").close(0)
+  if not bufs[2] then require("btw").open() end
+end
+
 return {
   {
     "letieu/btw.nvim",
     config = function(_, opts) require("btw").setup(opts) end,
+  },
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          ["<Leader>c"] = { openDashboard, desc = "Close buffer exept current" },
+        },
+      },
+    },
   },
 }
