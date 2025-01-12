686cb54c44b114b84fd3c2a63d71bae116f88f38
wert2all <wert2all@gmail.com>
2025-01-12 14:44:54 +0200

    fix: move neo-tree keybindings to neo-tree plugin settings

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,18 +1,27 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    popup_border_style = "single",
-    filesystem = {
-      filtered_items = {
-        hide_dotfiles = false,
-        hide_by_name = {
-          ".git",
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      popup_border_style = "single",
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_by_name = { ".git" },
+          always_show = { ".env" },
         },
-        always_show = {
-          ".env",
+      },
+      close_if_last_window = true,
+    },
+  },
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          ["<Leader>e"] = { "<cmd>Neotree float reveal<cr>", desc = "Toggle Explorer" },
+          ["<Leader>o"] = false,
         },
       },
     },
-    close_if_last_window = true,
   },
 }
