9c632a1bf4b79b934953f10321fd2e811597a279
wert2all <wert2all@gmail.com>
2025-04-09 20:35:05 +0300

    feat(colorscheme/astroui): Update colorscheme to monet and add sonokai and melange colorschemes as options in colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,10 +10,21 @@ return {
       vim.g.gruvbox_material_better_performance = 1
     end,
   },
+  {
+    "sainnhe/sonokai",
+    lazy = false,
+    config = function()
+      -- directly inside the plugin declaration.
+      vim.g.sonokai_enable_italic = true
+      vim.g.sonokai_style = "shusia"
+    end,
+  },
   { "wilmanbarrios/palenight.nvim", lazy = true },
   { "samharju/synthweave.nvim", lazy = true },
   { "folke/tokyonight.nvim", lazy = true },
   { "rebelot/kanagawa.nvim", lazy = true },
   { "vague2k/vague.nvim", lazy = true },
   { "EdenEast/nightfox.nvim", lazy = true },
+  { "fynnfluegge/monet.nvim", name = "monet", lazy = true },
+  { "savq/melange-nvim", lazy = true },
 }
