a652b99d2e86152fdab51904c80dfd2eb9e3c5f9
wert2all <wert2all@gmail.com>
2025-04-12 12:25:59 +0300

    feat: ensure markdown parser is installed for codecompanion

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,10 +1,16 @@
 return {
-
   {
     "olimorris/codecompanion.nvim",
     dependencies = {
       { "nvim-lua/plenary.nvim", branch = "master" },
-      "nvim-treesitter/nvim-treesitter",
+      {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+          if opts.ensure_installed ~= "all" then
+            opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "markdown" })
+          end
+        end,
+      },
     },
     opts = {
       strategies = {
@@ -23,7 +29,7 @@ return {
           return require("codecompanion.adapters").extend("openai_compatible", {
             env = {
               url = "https://openrouter.ai/api",
-              api_key = os.getenv("OPENROUTER_API_KEY"),
+              api_key = os.getenv "OPENROUTER_API_KEY",
               chat_url = "/v1/chat/completions",
             },
             schema = {
