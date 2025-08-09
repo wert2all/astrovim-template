66059307483ea23575304e57020514f1dddec04c
Serhii Shvets <wert2all@gmail.com>
2025-08-09 23:00:05 +0300

    fix: fix gruvbox theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,6 +8,8 @@ return {
       vim.g.gruvbox_material_background = "hard"
       vim.g.gruvbox_material_enable_italic = 1
       vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_cursor = "auto"
+      vim.g.gruvbox_material_statusline_style = "mix"
     end,
   },
   {
