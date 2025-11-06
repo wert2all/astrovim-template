6d4d938b5baa560721266644cd34547b75798635
Serhii Shvets <wert2all@gmail.com>
2025-11-06 17:31:10 +0200

    fix: update dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,7 +1,66 @@
 local btw = require "plugins.dashboard.btw"
+local D = require "plugins.dashboard.functions"
+
+local dev = [[
+ ######                
+ #     # ###### #    # 
+ #     # #      #    # 
+ #     # #####  #    # 
+ #     # #      #    # 
+ #     # #       #  #  
+ ######  ######   ##   
+]]
+local forge = [[
+                                   
+ ▄▄▄▄▄▄                            
+ █       ▄▄▄    ▄ ▄▄   ▄▄▄▄   ▄▄▄  
+ █▄▄▄▄▄ █▀ ▀█   █▀  ▀ █▀ ▀█  █▀  █ 
+ █      █   █   █     █   █  █▀▀▀▀ 
+ █      ▀█▄█▀   █     ▀█▄▀█  ▀█▄▄▀ 
+                       ▄  █        
+                        ▀▀         
+]]
 
 return {
   btw,
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = dev,
+        },
+        sections = {
+          {
+            pane = 1,
+            section = "terminal",
+            height = 25,
+            cmd = "cat | ~/.config/nvim/ansi/" .. D.getArt() .. ".sh",
+          },
+          { section = "header", pane = 2 },
+          {
+            pane = 2,
+            icon = D.getIcon(),
+            desc = D.getGreeting(),
+            padding = 5,
+          },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = function() return Snacks.git.get_root() ~= nil end,
+            cmd = "git status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
+        },
+      },
+    },
+  },
+
   {
     "AstroNvim/astrocore",
     opts = {
@@ -12,8 +71,8 @@ return {
               local bufs = vim.fn.getbufinfo { buflisted = 1 }
               require("astrocore.buffer").close(0)
               if not bufs[2] then
-                require("btw").open()
-                 -- require("snacks").dashboard()
+                -- require("btw").open()
+                require("snacks").dashboard()
               end
             end,
             desc = "Close buffer",
