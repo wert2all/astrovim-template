01593bbe2e029932319a37371869b85d0dd0620a
Serhii Shvets <wert2all@gmail.com>
2026-01-10 13:05:17 +0200

    fix: fix obsidian.nvim

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -57,7 +57,7 @@ return {
       frontmatter = {
         func = function(note)
           -- This is equivalent to the default frontmatter function.
-          local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+          local out = { tags = note.tags }
           -- `note.metadata` contains any manually added fields in the frontmatter.
           -- So here we just make sure those fields are kept in the frontmatter.
           if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
@@ -70,7 +70,7 @@ return {
       },
       -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
       -- URL it will be ignored but you can customize this behavior here.
-      follow_url_func = vim.ui.open,
+      -- follow_url_func = vim.ui.open,
     })
   end,
 }
