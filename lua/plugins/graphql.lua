aeaea60701677b13e444425613ddbab04a619927
Serhii Shvets <wert2all@gmail.com>
2026-02-05 01:14:22 +0200

    feat: ad gql support

---
+++ lua/plugins/graphql.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, { "graphql-language-service-cli" })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, { "graphql-language-service-cli" })
+    end,
+  },
+}
